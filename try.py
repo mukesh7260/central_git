7adb1ee926fa429512643884718b6eb715573501
Mukesh Kumar <58877260+mukesh7260@users.noreply.github.com>
2022-12-02 16:45:50 +0530

    ans of question no. 1 & 2 in try.py file

---
+++ try.py
@@ -0,0 +1,35 @@
+# s = list({5,6,1,2,3,4})
+# d = min(s)
+# print(d)
+
+
+# t = list({1,2,3,4})
+# b = min(t)
+# print(b) 
+
+
+# c = list({2,1})
+# f = min(c)
+# print(f) 
+
+
+
+# str1 = 'Quantom'
+# str2 = str1.replace('o','u') 
+# for e in str1:
+#     if e not in  str2:
+#         print(len(e))
+
+
+# str1 = 'Week Experience'
+# str2 = str1.replace('ee','or')
+# count = ""
+# for i in str2:
+#     if i not in str1:
+#         count = count + i+ "r" 
+# print(len(count))
+
+
+ 
+
+
